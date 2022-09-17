645bec80116f089c35bb97694922101523007fa2
brunnuscz <brunocruz012013@gmail.com>
2022-09-17 12:52:21 -0300

    Adicionando projeto map

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="img/favicon.png">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="public/css/style.css">
     <title>Map Color</title>
 </head>
 <body>
     <?php 
-        include("img/brasil-mapa.svg");
+        include("public/img/brasil-mapa.svg");
     ?>
 </body>
-<script src="js/script.js"></script>
+<script src="public/js/script.js"></script>
 </html>
